a95a2d479dba739b092fcea9b54e6906cfdb3613
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2022-06-25 13:57:44 -0300

    Ajustando arquivos para configuração do neovim

--- init.lua
+++ init.lua
@@ -5,5 +5,6 @@ require('statusline')
 require('paq') {
   'preservim/nerdtree';
   'vim-scripts/auto-pairs-gentle';
-  
+  'norcalli/snippets.nvim';
+	'nvim-treesitter/nvim-treesitter';
 }
--- lua/settings.lua
+++ lua/settings.lua
@@ -1,6 +1,7 @@
 local global = vim.g
 local opt = vim.opt
 local cmd = vim.cmd
+local u = require('utils')
 
 -- global config
 
@@ -15,18 +16,23 @@ opt.synmaxcol = 240
 
 opt.number = true
 opt.wrap = false
-opt.signcolumn = 'yes'
+opt.signcolumn = 'no'
 opt.showmatch = true
-opt.showmode = false
+opt.showmode = true
 opt.foldmethod = 'marker'
-opt.splitright = true
-opt.splitbelow = true
+opt.splitright = false
+opt.splitbelow = false
 opt.conceallevel = 0
-opt.colorcolumn = '80'
-opt.cursorline = true
 opt.scrolloff = 10
-opt.expandtab = true
+opt.expandtab = false
 opt.shiftwidth = 2
 opt.tabstop = 2
 opt.smartindent = true
 opt.shortmess:append {c = true}
+
+--u.create_augroup({
+--  { 'BufRead,BufNewFile', '/tmp/nail-*', 'setlocal', 'ft=mail' },
+--  { 'BufRead,BufNewFile', '*s-nail-*', 'setlocal', 'ft=mail' }
+--}, 'ftmail')
+
+--cmd('au BufNewFile,BufRead * if &ft == "" | set ft=text | endif')
